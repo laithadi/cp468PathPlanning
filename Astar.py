8f473548e776aa8e2b132f3ec2437c60f297c842
Ethan Coulter <coltsyy@gmail.com>
2020-11-24 14:30:56 -0500

    Update Astar.py

--- Astar.py
+++ Astar.py
@@ -1,9 +1,35 @@
+import copy
+from utils import validMoves
+
 def Astar(current, goal, heuristic):
     """
     Takes the current state of the robots in the musuem
     and returns the path they take to get to the goal state
     using the Manhatton distance heuristic
     """
+    curr_state = copy.deepcopy(current)
+    goal_state = copy.deepcopy(goal)
+
+    poss_moves = []
+    open_list = []
+    closed_list = []
+    g_score = 0
+    f_score = 9999
+
+    # append the current state of the node into the open list
+    open_list.append(current)
+    
+    # Run this loop whil there is still possible moves in the list.
+    while len(open_list > 0):
+        temp = open_list[0]
+        moves = validMoves(temp) #switch validmoves to only take tmep which is the current node.
+        for move in moves:
+            temp_score = int(g_score + manhatton(move, goal))
+            if temp_score < f_score:
+                f_score = temp_score
+        
+
+
 
 def manhatton(current, goal):
     """
@@ -14,7 +40,6 @@ def manhatton(current, goal):
 
     for x in range(length):
         for y in range(length):
-            #to ignore the empty tile
             if current[x][y] != 0 or current[x][y] != 0:
                 (goalx, goaly) = get_index(goal, current[x][y])
                 manhatton += abs(x - goalx) + abs(y - goaly)
