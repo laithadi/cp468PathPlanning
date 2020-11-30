4bac98c1f845912b1f3d96f3601b8b5a3dd0801e
Laith Adi <laith_adi@hotmail.com>
2020-11-29 22:13:53 -0500

    Update whatever.py

--- whatever.py
+++ whatever.py
@@ -15,63 +15,65 @@ class Node:
 
 def aStar(start, finish):
 
-    open = []
-    closed = [] 
-    g_score = 0 
+    open_list = [] # node that we have NOT explored its moves 
+    closed_list = [] # nodes that we have explored its moves
+    g_score = 0  # the depth we add to the total cost 
 
-    # scores = {} 
+    temp_h = manhatton(start, finish) # heuristic of the start node 
+    node_start = Node(index= start, parent= None, g_score= g_score, h_score= temp_h) # node of the start position
 
-    temp_h = manhatton(start, finish)
-    node_start = Node(index= start, parent= None, g_score= g_score, h_score= temp_h)
+    node_target = Node(finish, None, 0, 0) # node for the rendezvous point
 
-    node_target = Node(finish, None, 0, 0)
+    open_list.append(node_start) # adding the starting position node to the open_list 
 
-    open.append(node_start)
-    # scores[node_start.index] = node_start.cost
+    while open_list: # as long as we have a node in the open_list
 
-    while open:
+        node_current = getCurrentNode(open_list) # select the node with the least cost in open_list 
 
-        node_current = getCurrentNode(open) 
+        if node_current.index == node_target.index: break # if we reached the rendezvous point, break 
 
-        if node_current.index == node_target.index: break
+        moves = validMoves(ROOM, node_current.index, ROOM_DIM) # get all the valid moves 
 
-        # print(node_current.index)
-        moves = validMoves(ROOM, node_current.index, ROOM_DIM)
+        g_score += 1 # increase the g_score by 1 as we expand the node_current 
 
-        g_score += 1
+        for move in moves: # for each move in the valid move for node_current 
+            node_successor = Node(index= move, parent= node_current, g_score= g_score, h_score= manhatton(move, finish)) # create node for child of node_current
 
-        for move in moves: 
-            node_successor = Node(index= move, parent= node_current, g_score= g_score, h_score= manhatton(move, finish))
+            if node_successor.index == node_target.index: return "YAY"
 
-            if node_successor not in open:
-                if node_successor not in closed: 
-                    open.append(node_successor)
+
+            if node_successor not in open_list: # we have not come accross this node 
+                if node_successor not in closed_list:  
+                    open_list.append(node_successor) # add to the open_list 
                 else: continue
             
             else: continue
 
 
-        open.remove(node_current)
-        closed.append(node_current)
+        open_list.remove(node_current) # remove the node_current to not visit it again 
+        closed_list.append(node_current) # to keep track of nodes expanded 
 
-    path = [] 
-    while node_current.parent:
-        path.append(node_current)
-        node_current = node_current.parent
+    path = [] # the path 
+    while node_current.parent: # while there exists a node to back track 
+        path.append(node_current) # add node to path 
+        node_current = node_current.parent # move on to the next node 
     
-    return path
+    return path 
 
             
 
 
-def getCurrentNode(open):
+def getCurrentNode(open_list):
+    """
+    returns the node with the lowest cost 
+    """
 
     temp = 99999 
 
-    for node in open:
-        if node.cost <= temp:
-            currNode = node 
-            temp = node.cost
+    for node in open_list: # each node in the open_list
+        if node.cost <= temp: # if the cost of the node is less than temp 
+            currNode = node  # node becomes the new currNode 
+            temp = node.cost # temp gets updated to the currNode cost 
 
     return currNode 
 
