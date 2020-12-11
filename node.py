4fe704db2e08049d7257892e23410dcf58447af8
Laith Adi <laith_adi@hotmail.com>
2020-12-11 15:00:51 -0500

    Update node.py

--- node.py
+++ node.py
@@ -3,7 +3,6 @@ class Node:
     Node class to is to help us keep track of a coordinates index, parent (previous move or coordinate),
     g(n), h(n), and the cost f(n) = g(n) + h(n).
     """
-
     def __init__(self, index, parent, g_score, h_score):
         self.index = index
         self.parent = parent
